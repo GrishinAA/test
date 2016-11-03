2a68eaab3282e0a5c127bbbf0c92ad5daf19cf34
GrishinAA <irk-legioner@mail.ru>
2016-11-03 14:01:31 +0800

    hello world

---
+++ snake/snake/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+
+            Draw(x2, y2, sym1);
+
+
+
+
+            Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+    }
+}
